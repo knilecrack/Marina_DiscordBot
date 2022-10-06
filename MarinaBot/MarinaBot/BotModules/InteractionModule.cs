b854b4f46ab1bbfb8a9749e10a259b7927633230
kn1l3 <mcmausknile@hotmail.com>
2022-10-06 02:51:49 +0200

    testiram

---
+++ MarinaBot/MarinaBot/BotModules/InteractionModule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.Interactions;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+
+namespace MarinaBot.BotModules;
+public class InteractionModule : InteractionModuleBase<SocketInteractionContext>
+{
+
+  [SlashCommand("pali", "Dodaje pali na početak poruke")]
+  public Task HandlePaliAsync(string message)
+  {
+    return ReplyAsync($"Pali ***{message}*** brate narodnjaci su zakon");
+  }
+}
